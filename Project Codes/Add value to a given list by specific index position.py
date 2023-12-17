50b634cd91fd74ffa50b745ae6f415c5733b9c4b
AAYU6H <70193863+AAYU6H@users.noreply.github.com>
2023-12-17 07:17:06 +0000

    Make new codes

---
+++ Project Codes/Add value to a given list by specific index position.py
@@ -0,0 +1,10 @@
+# Topic : WAP to add any value to a given list by specific index position.
+
+lst = ["Hello", "World", "This", "is", "Python"]
+print(*lst, sep=", ")
+
+insertValue = "not"
+
+print("After inserting a new value : ")
+lst.insert(4, insertValue)
+print(*lst)
---
+++ Project Codes/Print all elements of a list.py
@@ -0,0 +1,5 @@
+# Topic : WAP to print all the elements of a list
+
+list = ["Onion", "Tomato", "Garlic", "Lemon", "Fruits"]
+ 
+print(*list, sep=", ")
