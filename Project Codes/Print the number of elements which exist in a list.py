501d912c675a011e28887e4526d6037d20295234
AAYU6H <70193863+AAYU6H@users.noreply.github.com>
2023-12-18 17:42:39 +0000

    Make changes to code files

--- Project Codes/Print the number of elements which exist in a list.py
+++ Project Codes/Print the number of elements which exist in a list.py
@@ -1,5 +1,6 @@
 #Topic : WAP to print the number of elements which exist in a list.
 
 lst = ["Hello", "World", "I", "don't", "eat", "mangoes"]
+print(lst)
 
-print(len(lst))
+print("Length of list = ", len(lst))
